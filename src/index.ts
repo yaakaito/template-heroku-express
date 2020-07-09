60275705f494badb556ea326ee8383958dfdbfda
yaakaito <yaakaito@gmail.com>
2020-07-09 17:25:51 +0900

    feat: add pg

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import express from 'express'
 import { withMiddlewares } from './middlewares'
+import { pool } from './db/driver'
+
 const port =  process.env.PORT || 11000
 
 const app = withMiddlewares(express())
 app.get('/', (request, response, next) => {
     (async () => {
+        const client = await pool.connect()
+        const { rows } = await client.query('select * from logs')
         response.send({
-            title: 'api',
-            body: process.env.TEXT,
+            rows
         })
     })().catch(next)
 })
